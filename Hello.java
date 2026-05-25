f3a4f8e79d89dddc38b158c57f1d4e51e220ec11
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-05-25 23:25:06 +0600

    add

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello{
     public static void main(String args[]){
         System.out.println("Hello World");
+        int num=45;
+        int num1=56;
+
+        System.out.println(num+num1);
     }
 }
