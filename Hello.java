865c20c1cfc16c0b4ee3f01f4934f561ab0a2dca
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-05-26 00:20:33 +0600

    new knowledge

--- Hello.java
+++ Hello.java
@@ -7,5 +7,21 @@ class Hello{
         System.out.println(num+num1);
         System.out.println(num);
         System.out.println(num1);
+
+        int nam=25;
+        short sh=266;
+        byte by=127;
+        long l=1258l;
+
+        double dh=56.25;
+        float ft=589.25f;
+
+        char ch='k';
+        boolean b=true;
+
+        int num2=0x7E;
+        System.out.println(num2);
+
+
     }
 }
