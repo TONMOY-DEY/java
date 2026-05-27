3759a34050bee2b55c7f43a0068ae3064587c6b9
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-05-27 19:38:34 +0600

    add

---
+++ if_else.java
@@ -0,0 +1,15 @@
+public class if_else {
+    public static void main(String[] args) {
+        int x=4;
+        int y=5;
+
+        if(x<10 && y<1){
+            System.out.println(true);
+        }
+        else{
+
+        System.out.println(false);
+        }
+    }
+    
+}
