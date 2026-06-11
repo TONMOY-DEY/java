ca64bc3f9e51d8528bc9ad0f091adf1df4b118dd
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-06-11 15:18:30 +0600

    looping

---
+++ Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+    public static void main(String[] args) {
+        
+
+        for(int i=0;i<4;i++){
+            System.out.println("Hello "+i);
+
+        }
+
+        for(int i=4;i>=1;i--){
+            System.out.println(i);
+
+        }
+    }
+    
+}
