6759582be8146063079d07bc14ed0e2619749c22
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-06-11 18:20:11 +0600

    class

--- oop.java
+++ oop.java
@@ -3,7 +3,7 @@ class calculator{
 
     public int add(int n1,int n2){
         int sum=n1+n2;
-         return sum;
+           return sum;
         //System.out.println("Hi there ,I am Backend Engineer");
         //return 0;
 
