3f9f4d7203bd8b6eab3118c7cbb1788490879a80
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-06-11 11:57:53 +0600

    initial commit

---
+++ Switch.java
@@ -0,0 +1,29 @@
+public class Switch {
+    public static void main(String[] args) {
+        
+    
+    int n=3;
+    switch(n){
+        case 1:
+            System.out.println("Monday");
+            break;
+
+        case 2:
+            System.out.println("tuesday");
+            break;
+
+        case 3:
+            System.out.println("wednesday");
+            break;
+
+        case 4:
+            System.out.println("thusday");
+            break;
+        
+        case 5:
+         System.out.println("Friday");
+
+    }
+    
+    }
+}
--- if_else.java
+++ if_else.java
@@ -2,13 +2,17 @@ public class if_else {
     public static void main(String[] args) {
         int x=4;
         int y=5;
+        int z=6;
 
         if(x<10 && y<1){
-            System.out.println(true);
+            System.out.println(x);
         }
-        else{
+        else if(x<z && y<z){
+        System.out.println(z);
+        }
 
-        System.out.println(false);
+        else{
+            System.out.println(y);
         }
     }
     
