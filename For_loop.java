58b5115c05f574be137c4518ec2f8b4ac8fae000
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-06-25 02:47:51 +0600

    for loop

---
+++ For_loop.java
@@ -0,0 +1,15 @@
+public class For_loop {
+    public static void main(String[] args) {
+        int num[]=new int[4];
+        num[0]=0;
+        num[1]=1;
+        num[2]=2;
+        num[3]=3;
+
+        for(int i=0;i<4;i++){
+            System.out.println(num[i]);
+        }
+
+    }
+    
+}
