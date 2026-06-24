04688f0c132b03556ff77291029b5bdccb373d95
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-06-24 23:26:53 +0600

    file add

--- Method23.java
+++ Method23.java
@@ -4,15 +4,22 @@ class computer{
     }
 
     public String getpen(int cost){
+        if(cost>10)
         return "pen";
-    }
+    
+    else 
+        return "Nothing";
+}
+    
 }
 
 public class Method23 {
     public static void main(String[] args) {
         computer co=new computer();
         co.playmusic();
-        co.getpen(10);
+        co.getpen(2);
+        System.out.println(co.getpen(2));
     }
     
 }
+
