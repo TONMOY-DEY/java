66c5929eba827a4da075683db7f1b9507ffa3412
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-07-30 14:09:12 +0600

    interface

---
+++ Interface25.java
@@ -0,0 +1,15 @@
+interface Animal {
+    void sound(); 
+}
+
+class Cat implements Animal{
+    public void sound(){
+        System.out.println("Bark");
+    }
+
+    public static void main(String[] args) {
+       Cat cat=new Cat();
+        cat.sound();
+    }
+}
+
